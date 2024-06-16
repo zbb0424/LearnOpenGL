38d94258e92ed72239b41f177c4113933fc679f5
嘉然我真的好喜欢你啊 <599033192@qq.com>
2024-06-16 15:42:23 +0800

    VAO
    设置vao

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -113,6 +113,10 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -140,6 +144,10 @@ int main(void)
 		2,3,0
 	};
 
+	unsigned int VAO;
+	GLCall(glGenVertexArrays(1, &VAO));
+	GLCall(glBindVertexArray(VAO));
+
 	/*复制顶点数组到缓冲中供OpengGL使用*/
 	unsigned int VBO;
 	GLCall(glGenBuffers(1, &VBO));
@@ -147,7 +155,7 @@ int main(void)
 	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
 	/*设置顶点属性指针*/
-	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));//0为position
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));//0为position 给VAO指定一个顶点布局
 	GLCall(glEnableVertexAttribArray(0));//enable启用0
 
 	unsigned int ibo;
@@ -160,10 +168,17 @@ int main(void)
 
 	/*使用shader程序*/
 	GLCall(glUseProgram(shader));
-	 
+	
+	/*uniform是全局的，是有别于顶点属性的另一种从cpu传递数据到gpu上的着色器的方式*/
 	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
 	ASSERT(location != -1);
-	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+	//GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+	/*清理所有绑定*/
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
 	float r = 0.0f;
 	float increment = 0.05f;
@@ -173,7 +188,12 @@ int main(void)
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUseProgram(shader));//重新启用shader
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+		GLCall(glBindVertexArray(VAO));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		if (r > 1.0f)
